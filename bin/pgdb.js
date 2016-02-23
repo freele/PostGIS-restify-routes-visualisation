8255d0d050a16b8f9fbd4f6ceece4450e2c8e3ba
Alexander Kremenets <kremenets0207@gmail.com>
2016-02-23 16:44:45 +0300

    Cleanup, selecting routes

--- bin/pgdb.js
+++ bin/pgdb.js
@@ -1,98 +1,55 @@
-var config     = require('./config.js'),
-    pg         = require('pg-query'),
-    path       = require('path');
-var table_name = config.get('table_name');
+var    pg         = require('pg-query');
+var    path       = require('path');
 
-pg.connectionParameters = config.get('db_config');
+pg.connectionParameters = 'postgres://docker:docker@192.168.99.100:25432/gis'; // _TODO move to config file
 console.log(pg.connectionParameters);
 
 var error_response = "data already exists - bypassing db initialization step\n";
 
-function createDBSchema(err, rows, result) {
-  if(err){
-    if(err.code == "ECONNREFUSED"){
-      return console.error("DB connection unavailable, see README notes for setup assistance\n", err);
-    }else{
-      return console.error(err);
-    }
-  }
-  var query = "CREATE TABLE "+table_name+" ( gid serial NOT NULL, name character varying(240), the_geom geometry, CONSTRAINT "+table_name+ "_pkey PRIMARY KEY (gid), CONSTRAINT enforce_dims_geom CHECK (st_ndims(the_geom) = 2), CONSTRAINT enforce_geotype_geom CHECK (geometrytype(the_geom) = 'POINT'::text OR the_geom IS NULL),CONSTRAINT enforce_srid_geom CHECK (st_srid(the_geom) = 4326) ) WITH ( OIDS=FALSE );";
-  pg(query, addSpatialIndex);
-};
 
-function addSpatialIndex(err, rows, result) {
-  if(err) {
-    return console.error(error_response, err);
-  }
-  pg("CREATE INDEX "+table_name+"_geom_gist ON "+table_name+" USING gist (the_geom);", importMapPoints);
-}
+// function select_box(req, res, next){
+//   //clean these variables:
+//   var query = req.query;
+//   var limit = (typeof(query.limit) !== "undefined") ? query.limit : 40;
+//   if(!(Number(query.lat1)
+//     && Number(query.lon1)
+//     && Number(query.lat2)
+//     && Number(query.lon2)
+//     && Number(limit)))
+//   {
+//     res.send(500, {http_status:400,error_msg: "this endpoint requires two pair of lat, long coordinates: lat1 lon1 lat2 lon2\na query 'limit' parameter can be optionally specified as well."});
+//     return console.error('could not connect to postgres', err);
+//   }
+//   pg('SELECT gid,name,ST_X(the_geom) as lon,ST_Y(the_geom) as lat FROM ' + table_name+ ' t WHERE ST_Intersects( ST_MakeEnvelope('+query.lon1+", "+query.lat1+", "+query.lon2+", "+query.lat2+", 4326), t.the_geom) LIMIT "+limit+';', function(err, rows, result){
+//     if(err) {
+//       res.send(500, {http_status:500,error_msg: err})
+//       return console.error('error running query', err);
+//     }
+//     res.send(rows);
+//     return rows;
+//   })
+// };
 
-function importMapPoints(err, rows, result) {
-  if(err) {
-    return console.error(error_response, err);
-  }
-  var points = require(path.resolve('./parkcoord.json'));
-  var insert = "Insert into "+table_name+" (name, the_geom) VALUES ";
-  var qpoints = points.map(insertMapPinSQL).join(",");
-  var query = insert + qpoints + ';';
-  console.log(query);
-  pg(query, function(err, rows, result) {
-    if(err) {
-      return console.error(error_response, err);
-    }
-    var response = 'Data import completed!';
-    return response;
-  });
-};
-
-function insertMapPinSQL(pin) {
-  var query = '';
-  var escape = /'/g
-  
-  if(typeof(pin) == 'object'){
-    query = "('" + pin.Name.replace(/'/g,"''") + "', ST_GeomFromText('POINT(" + pin.pos[0] +" "+ pin.pos[1] + " )', 4326))";  
-  }
-  return query;
-};
-
-function init_db(){
-  pg('CREATE EXTENSION postgis;', createDBSchema);
-} 
-
-function flush_db(){
-  pg('DROP TABLE '+ table_name+';', function(err, rows, result){
-    var response = 'Database dropped!';
-    console.log(response);
-    return response;
-  });
-} 
-
-function select_box(req, res, next){
-  //clean these variables:
-  var query = req.query;
-  var limit = (typeof(query.limit) !== "undefined") ? query.limit : 40;
-  if(!(Number(query.lat1) 
-    && Number(query.lon1) 
-    && Number(query.lat2) 
-    && Number(query.lon2)
-    && Number(limit)))
-  {
-    res.send(500, {http_status:400,error_msg: "this endpoint requires two pair of lat, long coordinates: lat1 lon1 lat2 lon2\na query 'limit' parameter can be optionally specified as well."});
-    return console.error('could not connect to postgres', err);
-  }
-  pg('SELECT gid,name,ST_X(the_geom) as lon,ST_Y(the_geom) as lat FROM ' + table_name+ ' t WHERE ST_Intersects( ST_MakeEnvelope('+query.lon1+", "+query.lat1+", "+query.lon2+", "+query.lat2+", 4326), t.the_geom) LIMIT "+limit+';', function(err, rows, result){
-    if(err) {
-      res.send(500, {http_status:500,error_msg: err})
-      return console.error('error running query', err);
-    }
-    res.send(rows);
-    return rows;
-  })
-};
 function select_all(req, res, next){
-  console.log(pg);
-  pg('SELECT gid,name,ST_X(the_geom) as lon,ST_Y(the_geom) as lat FROM ' + table_name +';', function(err, rows, result) {
-    console.log(config);
+  console.log('SELECT ALL');
+  pg(" \
+SELECT row_to_json(fc) \
+FROM \
+  (SELECT 'FeatureCollection' AS TYPE, \
+          array_to_json(array_agg(f)) AS features \
+   FROM \
+     (SELECT 'Feature' AS TYPE, \
+             ST_AsGeoJSON(lg.wkb_geometry)::json AS geometry, \
+             row_to_json( \
+                           (SELECT l \
+                            FROM \
+                              (SELECT time) AS l)) AS properties \
+      FROM ogrgeojson AS lg, \
+           malls \
+      WHERE ST_Crosses(ST_Buffer(ST_MakePoint(37.650324, 55.725506)::geography, 3000)::geometry, lg.wkb_geometry)) AS f) AS fc \
+      ",
+      function(err, rows, result) {
+    console.log('ERR', err);
     if(err) {
       res.send(500, {http_status:500,error_msg: err})
       return console.error('error running query', err);
@@ -104,7 +61,7 @@ function select_all(req, res, next){
 
 module.exports = exports = {
   selectAll: select_all,
-  selectBox: select_box,
-  flushDB:   flush_db,
-  initDB:    init_db
+  // selectBox: select_box,
+  // flushDB:   flush_db,
+  // initDB:    init_db
 };
--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-var restify = require('restify'),
-    fs      = require('fs'),
-    config  = require('./bin/config.js'),
-    db      = require('./bin/db.js');
+var restify = require('restify');
+var fs      = require('fs');
 var app     = restify.createServer();
-
-db.initDB('keepAlive');
+var db      = require('./bin/pgdb.js');
 
 app.use(restify.queryParser())
 app.use(restify.CORS())
 app.use(restify.fullResponse())
 
 // Routes
-app.get('/parks/within', db.selectBox);
-app.get('/parks', db.selectAll);
+// app.get('/routes/within', db.selectBox);
+app.get('/routes', db.selectAll);
 app.get('/status', function (req, res, next)
 {
   res.send("{status: 'ok'}");
@@ -28,6 +25,6 @@ app.get('/', function (req, res, next)
 
 app.get(/\/(css|js|img)\/?.*/, restify.serveStatic({directory: __dirname+'/static'}));
 
-app.listen(config.get('PORT'), config.get('IP'), function () {
-  console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT') )
+app.listen(3000, function () {
+  console.log( "Listening on " + "localhost "+ ", port " + 3000 )
 });
